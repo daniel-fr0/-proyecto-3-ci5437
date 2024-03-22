5b7b67c4b0fb7c8b58177dcf3eab57d6f1e0d0c2
daniel-fr0 <64498365+daniel-fr0@users.noreply.github.com>
2024-03-22 10:27:11 -0400

    probando, bug con partidos con el mismo equipo

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.JSON_to_SAT import getTournamentInfo, generateCNF
+from src.SAT_to_calendar import solve, getCalendar
+
+info = getTournamentInfo("torneo.json")
+generateCNF(info, "torneo.cnf")
+sol = solve("glucose", "torneo.cnf")
+calendario = getCalendar(info, sol)
+
+for event in sorted(list(calendario.events), key=lambda x: x.begin):
+	print(f'{event.name}\t\t{event.begin.strftime("%d/%m")}\t\tdesde las {event.begin.strftime("%H")} hasta las {event.end.strftime("%H")}')
